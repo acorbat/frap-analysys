fa6e1931325f93df46d36b977b573aac87eb20ff
acorbat <agustin.corbat@gmail.com>
2017-01-06 15:28:12 -0300

    src only contains general functions

--- src/Frap_Analysis.py
+++ src/Frap_Analysis.py
@@ -14,7 +14,7 @@ from scipy.optimize import curve_fit
 from skimage.filters import threshold_otsu
 from scipy.ndimage.morphology import binary_opening
 
-os.chdir(r'C:\Users\Agus\Documents\Laboratorio\uVesiculas\Analisis')
+os.chdir(r'C:\Users\Agus\Documents\Laboratorio\uVesiculas\FRAP_Analysis\src')
 import oiffile as oif
 
 #%% Define useful functions
@@ -123,7 +123,7 @@ def get_size(filepath):
 
 # Functions to crop and mask images
 
-def crop_and_conc(cell):
+def crop_and_conc(cell, FileDict):
     file_pre = FileDict[cell, 'pre']
     file_post = FileDict[cell, 'post']
     file_ble = file_post.replace('post', 'ble')
@@ -221,13 +221,15 @@ def calculate_fluorescence(CP, GR):
 # Functions that create and add columns to pandas dataframe
 
 def generate_df(fp):
+    FileDict = generate_FileDict(fp)
+    
     cells = set()
     for key in FileDict.keys():
         cells.add(key[0])
     
     df = pd.DataFrame()
     for cell in cells:
-        series, tot_Int = crop_and_conc(cell)
+        series, tot_Int = crop_and_conc(cell, FileDict)
         timepoint = get_timepoint(FileDict[cell, 'post'])
         df = df.append({'cell':cell, 'series':series, 'timepoint':timepoint, 'total_Int':tot_Int}, ignore_index=True)
     return df
@@ -324,39 +326,3 @@ def add_fitParams(df, Plot=False):
     df['tau'] = taus
     
     return df
-
-
-#%% Define folders and constants
-
-fp = r'C:\Users\Agus\Documents\Laboratorio\uVesiculas\Primeras Mediciones\2016-09-30\Smaug'
-
-#%% Generate dictionary of files to be analyzed
-
-FileDict = generate_FileDict(fp)
-df = generate_df(fp)
-
-#%% add time series of  masks
-
-df = add_foregroundSeries(df)
-
-#%% add fluorescence time series
-
-df =  add_fluorescence(df)
-
-#%% Calculate and add corrected fluorescence normalized to initial FRAP intensity
-
-df = add_f_corr(df)
-
-#%% Fit with FRAP typical function
-
-df =  add_fitParams(df, Plot=True)
-
-#%% cells not working
-
-non_cell = ['C3F2', 'C7F4', 'C7F2', 'C5F4', 'C6F1', 'C1F1', 'C4F1', 'C4F2', 'C3F3', 'C2F5', 'C6F5', 'C3F4', 'C3F1', 'C5F3', 'C1F5', 'C2F4', 'C2F1']
-
-corr_df = pd.DataFrame()
-
-for i in df.index:
-    if df['cell'][i] not in non_cell:
-        corr_df = corr_df.append({'Amp':df['Amp'][i], 'Imm':df['Imm'][i], 'k':1/df['tau'][i], 'area':df['pre_area'][i], 'pre_I':df['pre_I'][i]}, ignore_index=True)
